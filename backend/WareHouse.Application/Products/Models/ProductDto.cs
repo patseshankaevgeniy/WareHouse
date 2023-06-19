b14671f6ab8499921c10c5a092c6e62eedb6f9fa
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2023-06-19 11:59:03 +0300

    backend: added product endpoint

---
+++ backend/WareHouse.Application/Products/Models/ProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WareHouse.Application.Products.Models;
+
+public class ProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DepartmentId { get; set; }
+}
