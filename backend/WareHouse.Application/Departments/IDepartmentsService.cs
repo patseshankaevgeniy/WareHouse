3e4fe6c501aaf0db6791a9049837501dabdb93e4
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2023-06-19 11:52:40 +0300

    backend: added Department endpoint

---
+++ backend/WareHouse.Application/Departments/IDepartmentsService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WareHouse.Application.Departments.Models;
+
+namespace WareHouse.Application.Departments
+{
+    public interface IDepartmentsService
+    {
+        Task<List<DepartmentDto>> GetAllAsync();
+        Task<DepartmentDto> GetAsync(int id);
+        Task<DepartmentDto> CreateAsync(DepartmentDto word);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ backend/WareHouse.Application/Departments/Models/DepartmentAutoMapper.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using WareHouse.Domain.Entities;
+
+namespace WareHouse.Application.Departments.Models;
+
+public class DepartmentAutoMapper : Profile
+{
+    public DepartmentAutoMapper()
+    {
+        CreateMap<Department, DepartmentDto>().ReverseMap();
+    }
+}
