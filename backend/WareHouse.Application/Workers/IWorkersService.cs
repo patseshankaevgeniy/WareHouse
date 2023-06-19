2db6821f68ef0ca26b1e26386bbff01964489eca
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2023-06-19 11:55:56 +0300

    backend: added worker endpoint

---
+++ backend/WareHouse.Application/Workers/IWorkersService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WareHouse.Application.Workers.Models;
+
+namespace WareHouse.Application.Workers;
+
+public interface IWorkersService
+{
+    Task<List<WorkerDto>> GetAllAsync();
+    Task<WorkerDto> GetAsync(int id);
+    Task<WorkerDto> CreateAsync(WorkerDto word);
+    Task DeleteAsync(int id);
+}
---
+++ backend/WareHouse.Application/Workers/Models/WorkerDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace WareHouse.Application.Workers.Models
+{
+    internal class WorkerDtoValidator : AbstractValidator<WorkerDto>
+    {
+        public WorkerDtoValidator()
+        {
+            RuleFor(x=> x.FirstName).NotEmpty();
+            RuleFor(x=> x.LastName).NotEmpty();
+        }
+    }
+}
