baed2176ffe322a7ac9fd43b0b68387b00ab9fc6
patseshankaevgeniy <patseshanka.eugeniy@gmail.com>
2023-06-22 10:00:20 +0300

    backend: implement logging and exception middleware

---
+++ backend/WareHouse.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WareHouse.Application.Common.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(string message) : base(message)
+    {
+
+    }
+}
